046e94d62007f809eeb75b6dc046b1a0b83788bc
Beukot <michalek7790@gmail.com>
2025-11-03 20:26:42 +0100

    added dropdown menu with user related stuff

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,21 +1,75 @@
 'use client';
 
-import {Tabs} from "@mantine/core";
+import {Avatar, Text, Group, Menu, Tabs, UnstyledButton} from "@mantine/core";
+import React, {forwardRef} from "react";
+
+interface UserButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  image: string;
+  name: string;
+  icon?: React.ReactNode;
+}
+
+const UserButton = forwardRef<HTMLButtonElement, UserButtonProps>(
+  ({ image, name, icon, ...others }: UserButtonProps, ref) => (
+    <UnstyledButton
+      ref={ref}
+      style={{
+        padding: 'var(--mantine-spacing-md)',
+        color: 'var(--mantine-color-text)',
+        borderRadius: 'var(--mantine-radius-sm)',
+      }}
+      {...others}
+    >
+      <Group>
+        <Avatar src={image} radius="xl" />
+
+        <div style={{ flex: 1 }}>
+          <Text size="sm" fw="500">
+            {name}
+          </Text>
+        </div>
+      </Group>
+    </UnstyledButton>
+  )
+);
 
 export function Navbar() {
   return (
-    <Tabs defaultValue="map">
-      <Tabs.List>
-        <Tabs.Tab value="map">
-          Mapa
-        </Tabs.Tab>
-        <Tabs.Tab value="trips">
-          Wycieczki
-        </Tabs.Tab>
-        <Tabs.Tab value="activities">
-          Aktywności
-        </Tabs.Tab>
-      </Tabs.List>
-    </Tabs>
+    <>
+      <Tabs defaultValue="map">
+        <Tabs.List grow>
+
+          <Tabs.Tab value="map">
+            Map
+          </Tabs.Tab>
+
+          <Tabs.Tab value="trips">
+            Trips
+          </Tabs.Tab>
+
+          <Tabs.Tab value="activities">
+            Activities
+          </Tabs.Tab>
+
+          <Menu withArrow>
+            <Menu.Target>
+              <UserButton
+                image="https://http.cat/images/426.jpg"
+                name="Harriette Spoonlicker"
+              />
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item>
+                Settings
+              </Menu.Item>
+              <Menu.Item color="red">
+                Log out
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+
+        </Tabs.List>
+      </Tabs>
+    </>
   );
 }
