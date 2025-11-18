f6af657ee99b2343893eee3566f4b365aaa7aea7
Beukot <michalek7790@gmail.com>
2025-11-18 18:30:19 +0100

    improved activity post element

--- components/ActivitiesListElement/ActivitiesListElement.tsx
+++ components/ActivitiesListElement/ActivitiesListElement.tsx
@@ -1,23 +1,70 @@
 'use client';
 
-import {Card, Image, Text, Stack, ScrollArea} from '@mantine/core';
+import {Card, Image, Text, Stack, Flex, Group, SimpleGrid} from '@mantine/core';
 
-const ActivityPost = () => (
-  <Card shadow="sm" padding="lg" radius="md" withBorder w={300}>
-    <Card.Section>
-      <Image src="https://http.cat/images/404.jpg" h={100}/>
-    </Card.Section>
+interface ActivityData {
+  title: string;
+  desc: string;
+  distance: string;
+  time: string;
+  average: string;
+}
 
-    <Text fw={500} mt="md" mb="xs">
-      Wycieczka wgłąb torby
+const activities: ActivityData[] = [
+  {title: "Wycieczka wgłąb torby", desc: "Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!",
+  distance: "0.13 km", time: "00:00:32", average: "7.02 km/h"},{title: "Wycieczka wgłąb torby", desc: "Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!",
+  distance: "0.13 km", time: "00:00:32", average: "7.02 km/h"},{title: "Wycieczka wgłąb torby", desc: "Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!",
+  distance: "0.13 km", time: "00:00:32", average: "7.02 km/h"},{title: "Wycieczka wgłąb torby", desc: "Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!",
+  distance: "0.13 km", time: "00:00:32", average: "7.02 km/h"},{title: "Wycieczka wgłąb torby", desc: "Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!",
+  distance: "0.13 km", time: "00:00:32", average: "7.02 km/h"},{title: "Wycieczka wgłąb torby", desc: "Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!",
+  distance: "0.13 km", time: "00:00:32", average: "7.02 km/h"},{title: "Wycieczka wgłąb torby", desc: "Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!",
+  distance: "0.13 km", time: "00:00:32", average: "7.02 km/h"},{title: "Wycieczka wgłąb torby", desc: "Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!",
+  distance: "0.13 km", time: "00:00:32", average: "7.02 km/h"},{title: "Wycieczka wgłąb torby", desc: "Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!",
+  distance: "0.13 km", time: "00:00:32", average: "7.02 km/h"},{title: "Wycieczka wgłąb torby", desc: "Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!",
+  distance: "0.13 km", time: "00:00:32", average: "7.02 km/h"},
+]
+
+const ActivityStat = ({name, value} : {name: string; value: string;}) => (
+  <Stack gap="md">
+    <Text>
+      {name}
     </Text>
-
-    <Text size="sm" c="dimmed" style={{ whiteSpace: 'normal', wordBreak: 'break-word' }}>
-      Wycieczka wgłąb papierowej torby co w niej znajdziemy????? ja obstawiam że będzie tam kocimiętka!
+    <Text>
+      {value}
     </Text>
+  </Stack>
+)
 
+const ActivityPost = (
+  {data} : {data: ActivityData;}) => (
+  <Card shadow="sm" padding="xs" radius="md" withBorder h={100} w={1200}>
+    <Flex
+      direction="row"
+      gap="md"
+      justify="flex-start"
+      align="flex-start"
+      wrap="nowrap">
+      <Card.Section>
+        <Image src="https://http.cat/images/404.jpg" h={100} w={100}/>
+      </Card.Section>
 
+      <Stack align="flex-start" justify="center" gap="md">
+        <Text fw={500}>
+          {data.title}
+        </Text>
+        <Text size="sm" c="dimmed" style={{ whiteSpace: 'normal', wordBreak: 'break-word' }}>
+          {data.desc}
+        </Text>
+      </Stack>
+
+      <SimpleGrid cols={5}>
+        <ActivityStat name="Distance" value={data.distance} />
+        <ActivityStat name="Time" value={data.time} />
+        <ActivityStat name="Average" value={data.average} />
+      </SimpleGrid>
+    </Flex>
   </Card>
+
 );
 
 export function ActivitiesListElement() {
@@ -28,16 +75,7 @@ export function ActivitiesListElement() {
           justify="flex-start"
           gap="md"
         >
-          <ActivityPost/>
-          <ActivityPost/>
-          <ActivityPost/>
-          <ActivityPost/>
-          <ActivityPost/>
-          <ActivityPost/>
-          <ActivityPost/>
-          <ActivityPost/>
-          <ActivityPost/>
-          <ActivityPost/>
+          {activities.map((activity: ActivityData) => (<ActivityPost data={activity} />))}
         </Stack>
 
     </>
