2c519336762802d59c2c5c371ab2287477c158ab
Beukot <michalek7790@gmail.com>
2025-12-21 00:29:50 +0100

    added activity list to appshell

--- components/ActivitiesListElement/ActivitiesListElement.tsx
+++ components/ActivitiesListElement/ActivitiesListElement.tsx
@@ -3,15 +3,24 @@
 import { ActivityPost } from '@/components/ActivityPost/ActivityPost';
 import { PostsList } from '@/components/PostsList/PostsList';
 import { mockActivities } from '@/lib/mockData';
+import {AppShell, ScrollArea} from "@mantine/core";
 
 export function ActivitiesListElement() {
   return (
     <>
-      <PostsList
-        Content={mockActivities.map((activity) => (
-          <ActivityPost key={activity.id} data={activity} />
-        ))}
-      />
+      <AppShell.Navbar>
+        <AppShell.Section component={ScrollArea}>
+          <PostsList
+            Content={mockActivities.map((activity) => (
+              <ActivityPost key={activity.id} data={activity} />
+            ))}
+          />
+        </AppShell.Section>
+      </AppShell.Navbar>
+
+      <AppShell.Aside>
+        hej :3
+      </AppShell.Aside>
     </>
   );
 }
