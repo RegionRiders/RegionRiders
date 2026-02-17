b238b3814096fee1d0562de2c897e4dbb5c69fc1
Maksymilian Sobolewski <maksobo80@edu.gdansk.pl>
2026-02-17 12:24:18 +0100

    Added a button to add colors to a gradient

--- components/GradientColorPicker/GradientColorPicker.tsx
+++ components/GradientColorPicker/GradientColorPicker.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { ColorPicker, Modal, toRgba } from '@mantine/core';
+import { Button, ColorPicker, Modal, toRgba } from '@mantine/core';
 import { ColorThreshold } from '../ActivityMap/mapTypes';
 
 
@@ -22,7 +22,10 @@ export function GradientColorPicker() {
         format="rgba"
         value={`rgba(${colorThresholds[activeThresholdIndex].color.join(', ')})`}
         onChange={(color) => {
+          // Convert string value of the ColorPicker to an RGBA type object
           const rgba = toRgba(color);
+
+          // Update the ColorThreshold color value
           setColorThresholds((prev) => {
             const newThresholds = [...prev];
             newThresholds[activeThresholdIndex] = {
@@ -33,6 +36,11 @@ export function GradientColorPicker() {
           });
         }}
       />
+      <Button onClick={() => {
+        // Add a new color threshold to the array (by coping the last color) and set the color on the ColorPicker to the last added color
+        setColorThresholds((prev)  => [...prev, prev[prev.length - 1]])
+        setActiveThresholdIndex(colorThresholds.length - 1);
+      }}>Add Color</Button>
     </Modal>
   );
 }
