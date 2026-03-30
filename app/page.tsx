b71ea03193ce67168521ffa726a7ebf67de7eb2c
Beukot <michalek7790@gmail.com>
2026-03-30 18:10:44 +0200

    remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,4 @@
 import { Navbar } from '@/components/Navbar/Navbar';
-import {createTheme} from "@mantine/core";
-
-const theme = createTheme({
-  breakpoints: {
-    xs: '30em',
-    sm: '80em',
-    md: '64em',
-    lg: '74em',
-    xl: '90em',
-  },
-}); // i think this might be needed but im too drunk rn to remember what is it for
 
 export default function HomePage() {
   return (
